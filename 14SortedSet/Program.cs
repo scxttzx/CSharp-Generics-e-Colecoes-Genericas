594a56cd2a3f81c93f36cc90a682e67f9d753021
crfmattos <gabrielmbserra@gmail.com>
2024-09-17 13:41:53 -0300

    17/09

---
+++ 14SortedSet/Program.cs
@@ -0,0 +1,26 @@
+﻿
+var numeros = new SortedSet<int>(){ 1, 5, 3, 4, 2 };
+ExibirColecao(numeros);
+
+var frutas = new SortedSet<string> { "uva", "banana", "pera", "kiwi" };
+ExibirColecao(frutas);
+
+SortedSet<int> pares = new SortedSet<int>();
+for (int i = 6; i > 0;i--)
+{
+    pares.Add(i);
+}
+ExibirColecao(pares);
+
+var numeros2 = new List<int>() { 1, 7, 3, 6, 5, 4, 2 };
+SortedSet<int> conjunto = new SortedSet<int>(numeros2);
+ExibirColecao(conjunto);
+
+Console.ReadKey();
+
+static void ExibirColecao<T>(IEnumerable<T> colecao)
+{
+    Console.WriteLine();
+    foreach (var item in colecao)
+		Console.WriteLine(item);
+}
---
+++ 15Stack/Program.cs
@@ -0,0 +1,13 @@
+﻿//Stack<int> numeros = new Stack<int>();
+//numeros.Push(1);
+
+//int[] array1 = new int[] { 1, 2, 3, 4 };
+//Stack<int> pilha1 = new Stack<int>(array1);
+
+//var impares = new Stack<int>(3);
+//impares.Push(1);
+
+Stack<int> numeros = new Stack<int>();
+numeros.Push(10);
+
+Console.ReadKey();
